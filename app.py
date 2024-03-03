7c7aced169c599d15abcd03f8e032246dec45621
Carter <carter.gamble014@gmail.com>
2024-03-02 19:08:51 -0700

    Login page back

--- app.py
+++ app.py
@@ -4,21 +4,10 @@ from lib.templates.appshell import create_appshell
 from dash import Dash, html, dcc
 import dash_mantine_components as dmc
 
-VALID_USERNAME_PASSWORD_PAIRS = {
-    'connor1': 'abc123',
-    'carter1': 'abc123',
-    'cantera1': 'abc123'
-}
-
 app = Dash(
     __name__,
     use_pages=True,
     update_title=None,
-)
-
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
 )
 
 app.layout = dmc.MantineProvider(children=[dcc.Location(id='url'), create_appshell()])
--- pages/login.py
+++ pages/login.py
@@ -1,54 +1,53 @@
-# import dash
-# from dash import html, dcc, Input, callback, State, Output
-# import dash_mantine_components as dmc
-#
-# dash.register_page(__name__, path='/login', name='Login')
-#
-#
-# def create_login():
-#     return html.Div(
-#         children=[
-#             html.H1("Login"),
-#             html.Div(
-#                 children=[
-#                     dmc.TextInput(
-#                         label="Username",
-#                         id="username",
-#                         placeholder="Enter your username",
-#                         required=True,
-#                     ),
-#                     dmc.PasswordInput(
-#                         label="Password",
-#                         id="password",
-#                         placeholder="Enter your password",
-#                         required=True,
-#                     ),
-#                     dmc.Button(
-#                         "Login",
-#                         id="login-button",
-#                         color="blue",
-#                         variant="filled",
-#                         mt=5
-#                     ),
-#                 ]
-#             ),
-#         ]
-#     )
-#
-#
-# @callback(
-#     Output("login-button", "loading"),
-#     Input("login-button", "n_clicks"),
-#     State("username", "value"),
-#     State("password", "value"),
-#     prevent_initial_call=True
-# )
-# def login_submit(n_clicks, username, password):
-#     if n_clicks:
-#         # TODO: Add login logic, Auth, API call, etc.
-#         print(f"Username: {username}, Password: {password}")
-#         return True
-#     return False
-#
-#
-# layout = create_login()
+import dash
+from dash import html, dcc, Input, callback, State, Output
+import dash_mantine_components as dmc
+
+dash.register_page(__name__, path='/login', name='Login')
+
+
+def create_login():
+    return html.Div(
+        children=[
+            html.H1("Login"),
+            html.Div(
+                children=[
+                    dmc.TextInput(
+                        label="Username",
+                        id="username",
+                        placeholder="Enter your username",
+                        required=True,
+                    ),
+                    dmc.PasswordInput(
+                        label="Password",
+                        id="password",
+                        placeholder="Enter your password",
+                        required=True,
+                    ),
+                    dmc.Button(
+                        "Login",
+                        id="login-button",
+                        color="blue",
+                        variant="filled",
+                        mt=5
+                    ),
+                ]
+            ),
+        ]
+    )
+
+
+@callback(
+    Output("login-button", "loading"),
+    Input("login-button", "n_clicks"),
+    State("username", "value"),
+    State("password", "value"),
+    prevent_initial_call=True
+)
+def login_submit(n_clicks, username, password):
+    if n_clicks:
+        print(f"Username: {username}, Password: {password}")
+        return True
+    return False
+
+
+layout = create_login()
