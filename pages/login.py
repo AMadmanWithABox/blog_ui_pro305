b891863cbeadec3098fc1f3921d631805517591e
Carter <carter.gamble014@gmail.com>
2024-03-02 18:00:43 -0700

    Added content to login page

--- pages/login.py
+++ pages/login.py
@@ -1,5 +1,6 @@
 import dash
-from dash import html, dcc
+from dash import html, dcc, Input, callback, State, Output
+import dash_mantine_components as dmc
 
 dash.register_page(__name__, path='/login', name='Login')
 
@@ -10,15 +11,44 @@ def create_login():
             html.H1("Login"),
             html.Div(
                 children=[
-                    html.Label("Username"),
-                    dcc.Input(id="username-input", type="text"),
-                    html.Label("Password"),
-                    dcc.Input(id="password-input", type="password"),
-                    html.Button("Login", id="login-button"),
+                    dmc.TextInput(
+                        label="Username",
+                        id="username",
+                        placeholder="Enter your username",
+                        required=True,
+                    ),
+                    dmc.PasswordInput(
+                        label="Password",
+                        id="password",
+                        placeholder="Enter your password",
+                        required=True,
+                    ),
+                    dmc.Button(
+                        "Login",
+                        id="login-button",
+                        color="blue",
+                        variant="filled",
+                        mt=5
+                    ),
                 ]
             ),
         ]
     )
+
+
+@callback(
+    Output("login-button", "loading"),
+    Input("login-button", "n_clicks"),
+    State("username", "value"),
+    State("password", "value"),
+    prevent_initial_call=True
+)
+def login_submit(n_clicks, username, password):
+    if n_clicks:
+        print(f"Username: {username}, Password: {password}")
+        return True
+    return False
+
 
 
 layout = create_login()
