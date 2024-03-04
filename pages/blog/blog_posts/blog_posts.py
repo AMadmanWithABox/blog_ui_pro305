edd2eead25a07ba89176c66c8b4db600df3c1cf6
choffman4 <choffman@student.neumont.edu>
2024-03-03 18:02:17 -0700

    blog posts

--- pages/blog/blog_posts/blog_posts.py
+++ pages/blog/blog_posts/blog_posts.py
@@ -1,19 +1,44 @@
 import dash
-from dash import clientside_callback
+import requests
+from dash import html, dcc, Input, callback, State, Output
 
 from lib.templates.post_card import create_post_card
+
+
+from config import cache  # Make sure this import is correct
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+api_gateway = os.getenv('API_GATEWAY')
 
 dash.register_page(__name__, path_template="/blog/<blog_id>", name="Blog Posts", redirect_from=["/blogs"])
 
 
+@callback(
+
+)
 def layout(blog_id):
     # TODO: Get blog by id
-    blog = {
-        "title": f"Blog {blog_id}",
-        "category": f"Category {blog_id}",
-        "description": f"Description {blog_id}",
-        "id": blog_id
-    }
+    blog_response = requests.get(f"{api_gateway}/blog/posts/{blog_id}", auth=HTTPBasicAuth(cache.get("username"),
+                                                                                           cache.get("password")))
+    blog_posts = blog_response.json()
+
+    posts = []
+
+    for post in blog_posts:
+        post = {
+            "blog_id": post["blog_id"],
+            "Id": post["Id"],
+            "title": post["title"],
+            "content": post["content"]
+        }
+        posts.append(post)
+
+    print(posts)
+
     # TODO: Get blog posts by blog id
     unfiltered_posts = [{"title": f"title {i}", "content": (f"content {i}" for j in range(1, 1000))} for i in range(5)]
     filtered_posts = [create_post_card(post) for post in unfiltered_posts]
