31f579253150ee3ef5945ec74ce766ae76e995a7
Carter <carter.gamble014@gmail.com>
2024-03-04 21:02:30 -0700

    aaaa

--- pages/blog/blog_posts/create_post.py
+++ pages/blog/blog_posts/create_post.py
@@ -12,16 +12,60 @@ import os
 load_dotenv()
 api_gateway = os.getenv('API_GATEWAY')
 
-dash.register_page(__name__, path_template="/blog/<blog_id>", name="Blog Posts", redirect_from=["/blogs"])
+dash.register_page(__name__, path_template="/blog/<blog_id>/create_post", name="Create Post")
+
+layout = dmc.Container(
+    children=[
+        dmc.TextInput(
+            label="Title",
+            id="title",
+            placeholder="Enter the title of your post",
+            required=True,
+        ),
+        dmc.TextInput(
+            label="Category",
+            id="category",
+            placeholder="Enter the category of your post",
+            required=True,
+        ),
+        dmc.Textarea(
+            label="Content",
+            id="content",
+            placeholder="Enter the content of your post",
+            required=True,
+        ),
+        dmc.Button(
+            "Create Post",
+            id="create-post-button",
+            color="blue",
+            variant="filled",
+            mt=5
+        ),
+    ],
+    style={'width': '100%'}
+)
 
 
-def create_blog_page(blog_id):
-    db_response = requests.get(api_gateway + f"/blog/{blog_id}", auth=HTTPBasicAuth(cache.get("username"), cache.get("password")))
-    blog = db_response.json()
-    posts = blog['posts']
-    post_cards = [create_post_card(post) for post in posts]
-    return html.Div(post_cards)
-
-
-layout = html.Div(create_blog_page(blog_id))
-
+@callback(
+    Output("create-post-button", "loading"),
+    Output('url', 'pathname', allow_duplicate=True),
+    Input("create-post-button", "n_clicks"),
+    State("title", "value"),
+    State("category", "value"),
+    State("content", "value"),
+    prevent_initial_call=True,
+)
+def create_post(n_clicks, title, category, content):
+    if n_clicks:
+        print(f"Title: {title}, Category: {category}, Content: {content}")
+        try:
+            # Make a POST request with basic auth
+            response = requests.post(f"{api_gateway}/blog/{blog_id}/post",
+                                     json={"title": title, "category": category, "content": content},
+                                     auth=HTTPBasicAuth(cache.get("username"), cache.get("password")))
+            print(response.json())
+            return False, f"/blog/{blog_id}"
+        except Exception as e:
+            print(e)
+            return False, f"/blog/{blog_id}/create_post"
+    return False, f"/blog/{blog_id}/create_post"
